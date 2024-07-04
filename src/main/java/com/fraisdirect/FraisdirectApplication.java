c88275bdf28d252f6525d025d4c663649894a5ef
ciidii <booba167967@gmail.com>
2024-07-04 11:42:58 +0000

    initial commit

---
+++ src/main/java/com/fraisdirect/FraisdirectApplication.java
@@ -0,0 +1,13 @@
+package com.fraisdirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraisdirectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraisdirectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fraisdirect/FraisdirectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fraisdirect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraisdirectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
