c4b411e6811a8e87652766e3609cf63479169914
ciidii <booba167967@gmail.com>
2024-07-04 12:25:50 +0000

    initial commit 2

--- src/main/java/com/fraisdirect/Application.java
+++ src/main/java/com/fraisdirect/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FraisdirectApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FraisdirectApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
